384579a66bf245323072705024e0edb358501dcc
kangjs3779 <123609533+kangjs3779@users.noreply.github.com>
2023-04-07 17:48:17 +0900

    0407

---
+++ src/jisoo/programmers/level0/exam120834.java
@@ -0,0 +1,16 @@
+package jisoo.programmers.level0;
+
+import java.util.*;
+
+public class exam120834 {
+	public static void main(String[] args) {
+		String result = "cd";
+		int age = 23;
+		
+	
+		
+		
+		
+		
+	}
+}
---
+++ src/jisoo/programmers/level0/exam120846.java
@@ -0,0 +1,19 @@
+package jisoo.programmers.level0;
+
+import java.util.*;
+
+public class exam120846 {
+	public static void main(String[] args) {
+		int n = 10;
+		int result = 5;
+		
+		Map<Integer, Integer> map = new HashMap<>(n)
+		for(int a = 1; a <= n; a++) {
+			for(int b = 1; b <= a; b++) {
+				if((a % b) == 0) {
+					set
+				}
+			}
+		}
+	}
+}
