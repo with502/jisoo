57e9c08939145ddcb6f7fc6c2811fd5d06aee9cf
jisookkang <123609533+kangjs3779@users.noreply.github.com>
2023-04-14 10:54:06 +0900

    0414

--- src/jisoo/programmers/level0/exam120834.java
+++ src/jisoo/programmers/level0/exam120834.java
@@ -4,11 +4,26 @@ import java.util.*;
 
 public class exam120834 {
 	public static void main(String[] args) {
-		String result = "cd";
+		String re = "cd";
 		int age = 23;
 		
-	
+		List<Character> list = new ArrayList<>();
 		
+		for(char a = 'a'; a <= 'j' ; a++) {
+			list.add(a);
+		}
+		
+		boolean run = true;
+		String result = "a";
+		while(run) {
+			if((age % 10) < 10) {
+				result += list.get((age % 10));
+			} else {
+				run = false;
+			}
+		}
+		
+		System.out.println(result);
 		
 		
 		
---
+++ src/jisoo/programmers/level1/exam42576.java
@@ -0,0 +1,15 @@
+package jisoo.programmers.level1;
+
+import java.util.*;
+import java.util.function.IntFunction;
+
+public class exam42576 {
+	public static void main(String[] args) {
+		String[] participant = {"leo", "kiki", "eden"};
+		String[] completion = {"eden", "kiki"};
+		
+		Set<String> set = new HashSet<>();
+		
+		
+	}
+}
